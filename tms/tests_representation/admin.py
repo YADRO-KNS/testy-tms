6a22a7d53968209168ba8899e8235914842ec1b9
r.kabaev <Pfchfycbz2011>
2022-10-03 11:18:22 +0300

    Added admin page for test representations

---
+++ tms/tests_representation/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
+from tests_representation.models import TestPlan, Test, TestResult
+from tms.admin import BaseAdmin
+
+
+@admin.register(TestPlan)
+class TestPlanAdmin(BaseAdmin, MPTTModelAdmin):
+    list_display = ('parent', 'started_at', 'due_date', 'finished_at', 'is_archive')
+    search_fields = ('id',)
+
+
+@admin.register(Test)
+class TestAdmin(BaseAdmin):
+    list_display = ('case', 'plan', 'user', 'is_archive')
+    search_fields = ('id',)
+
+
+@admin.register(TestResult)
+class TestResultAdmin(BaseAdmin):
+    list_display = ('test', 'status', 'comment', 'user')
+    search_fields = ('name',)
