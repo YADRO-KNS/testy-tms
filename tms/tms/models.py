deeaad64516ebd37e245e3f9faf31b07087cdf30
Dmitry Tkach <d.tkach@yadro.com>
2022-10-05 13:04:15 +0300

    Added service model mixin for create and update models

--- tms/tms/models.py
+++ tms/tms/models.py
@@ -1,8 +1,47 @@
+from typing import Any, Dict, List, Tuple
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from tms.types import DjangoModelType
 
-class BaseModel(models.Model):
+
+class ServiceModelMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def model_create(cls, fields: List[str], data: Dict[str, Any], commit: bool = True) -> DjangoModelType:
+        actually_fields = {key: data[key] for key in fields}
+        instance = cls(**actually_fields)
+
+        if commit:
+            instance.full_clean()
+            instance.save()
+
+        return instance
+
+    def model_update(
+        self, fields: List[str], data: Dict[str, Any], commit: bool = True
+    ) -> Tuple[DjangoModelType, bool]:
+        has_updated = False
+
+        for field in fields:
+            if field not in data:
+                continue
+
+            if getattr(self, field) != data[field]:
+                has_updated = True
+                setattr(self, field, data[field])
+
+        if has_updated and commit:
+            self.full_clean()
+            self.save(update_fields=fields)
+
+        return self, has_updated
+
+
+class BaseModel(ServiceModelMixin, models.Model):
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
     updated_at = models.DateTimeField(_('updated at'), auto_now=True)
 
