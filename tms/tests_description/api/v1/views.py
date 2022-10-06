86031724045d48d59ac34a474bb3e381ff3312c5
Dmitry Tkach <d.tkach@yadro.com>
2022-10-06 18:24:38 +0300

    Removed HistoricalTestCaseViewSet

--- tms/tests_description/api/v1/views.py
+++ tms/tests_description/api/v1/views.py
@@ -1,15 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 
-from tests_description.api.v1.serializers import (HistoricalTestCaseSerializer,
-                                                  TestCaseSerializer,
+from tests_description.api.v1.serializers import (TestCaseSerializer,
                                                   TestSuiteSerializer)
 from tests_description.selectors.cases import TestCaseSelector
-from tests_description.selectors.historical_cases import \
-    HistoricalTestCaseSelector
 from tests_description.selectors.suites import TestSuiteSelector
 from tests_description.services.cases import TestCaseService
-from tests_description.services.historical_cases import \
-    HistoricalTestCaseService
 from tests_description.services.suites import TestSuiteService
 
 
@@ -22,20 +17,6 @@ class TestCaseViewSet(ModelViewSet):
 
     def perform_update(self, serializer: TestCaseSerializer):
         serializer.instance = TestCaseService().case_update(serializer.instance, serializer.validated_data)
-
-
-class HistoricalTestCaseViewSet(ModelViewSet):
-    queryset = HistoricalTestCaseSelector().historical_case_list()
-    serializer_class = HistoricalTestCaseSerializer
-
-    def perform_create(self, serializer: HistoricalTestCaseSerializer):
-        serializer.instance = HistoricalTestCaseService().historical_case_create(serializer.validated_data)
-
-    def perform_update(self, serializer: HistoricalTestCaseSerializer):
-        serializer.instance = HistoricalTestCaseService().historical_case_suite_update(
-            serializer.instance,
-            serializer.validated_data
-        )
 
 
 class TestSuiteViewSet(ModelViewSet):
