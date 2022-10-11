1796bb29b42e0b584b913dc2f19b5e3b6f9d7c2f
Dmitry Tkach <d.tkach@yadro.com>
2022-10-11 13:18:38 +0300

    Revert "Updated linter-ci.sh with exit_status"
    This reverts commit 17a6c55996f760297a4b8b33d03939e91f0c2444.

--- linter-ci.sh
+++ linter-ci.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-exit_status=0
+
+fails=""
+
+inspect() {
+  # Usage: inspect $? lint
+  # * param1: job result code
+  # * param2: job name
+  if [ $1 -ne 0 ]; then
+    fails="${fails} $2"
+  fi
+}
+
+lint() {
+  docker-compose -f docker-compose-ci.yml up -d --build
+  docker-compose -f docker-compose-ci.yml run --rm  tms-ci flake8 .
+  inspect $? lint
+  docker-compose -f docker-compose-ci.yml down -v
+}
 
 echo "Running linter!"
-docker-compose -f docker-compose-ci.yml up -d --build
-docker-compose -f docker-compose-ci.yml run --rm tms-ci flake8 .
-exit_status=$?
-docker-compose -f docker-compose-ci.yml down -v
+lint
 
-exit $exit_status
+if [ -n "${fails}" ]; then
+  echo "Linter failed: ${fails}"
+  exit 1
+else
+  echo "Linter passed!"
+  exit 0
+fi
