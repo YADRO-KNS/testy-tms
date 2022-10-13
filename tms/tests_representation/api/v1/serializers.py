025837a6d822bf38770937f36251887c035fcea8
Dmitry Tkach <d.tkach@yadro.com>
2022-10-13 10:03:23 +0300

    Added TestPlan serializers

--- tms/tests_representation/api/v1/serializers.py
+++ tms/tests_representation/api/v1/serializers.py
@@ -1,6 +1,9 @@
-from rest_framework.relations import HyperlinkedIdentityField
+from rest_framework.relations import HyperlinkedIdentityField, PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
-from tests_representation.models import Parameter, Test, TestResult
+
+from tests_description.selectors.cases import TestCaseSelector
+from tests_representation.models import Parameter, Test, TestResult, TestPlan
+from tests_representation.selectors.parameters import ParameterSelector
 
 
 class ParameterSerializer(ModelSerializer):
@@ -9,6 +12,38 @@ class ParameterSerializer(ModelSerializer):
     class Meta:
         model = Parameter
         fields = ('id', 'project', 'data', 'group_name', 'url')
+
+
+class TestPlanUpdateSerializer(ModelSerializer):
+    test_cases = PrimaryKeyRelatedField(queryset=TestCaseSelector().case_list(), many=True, required=False)
+
+    class Meta:
+        model = TestPlan
+        fields = (
+            'id', 'name', 'parent', 'test_cases', 'started_at', 'due_date', 'finished_at', 'is_archive',
+        )
+
+
+class TestPlanInputSerializer(ModelSerializer):
+    test_cases = PrimaryKeyRelatedField(queryset=TestCaseSelector().case_list(), many=True, required=False)
+    parameters = PrimaryKeyRelatedField(queryset=ParameterSelector().parameter_list(), many=True, required=False)
+
+    class Meta:
+        model = TestPlan
+        fields = (
+            'id', 'name', 'parent', 'test_cases', 'parameters', 'started_at', 'due_date', 'finished_at', 'is_archive',
+        )
+
+
+class TestPlanOutputSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(view_name='api:v1:testplan-detail')
+
+    class Meta:
+        model = TestPlan
+        fields = (
+            'id', 'name', 'parent', 'parameters', 'started_at', 'due_date', 'finished_at', 'is_archive',
+            'url', 'child_test_planes', 'tests'
+        )
 
 
 class TestSerializer(ModelSerializer):
