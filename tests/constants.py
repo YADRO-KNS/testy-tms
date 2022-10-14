4f550cd63c5ceae760d522e8b466dedfcc599ea6
r.kabaev <Pfchfycbz2011>
2022-10-14 10:35:14 +0300

    Removed version directory for models. Changed time now to timezone.now

--- tests/constants.py
+++ tests/constants.py
@@ -1,4 +1,4 @@
-import datetime
+from django.utils import timezone
 
 USERNAME = 'test_user'
 PASSWORD = '123qweasd123'
@@ -15,7 +15,7 @@ SETUP = '1.Setup\n2.Setup'
 SCENARIO = '1.break\n2.break\n3.break'
 TEARDOWN = '1.Teardown\n2.Teardown'
 ESTIMATE = 12345
-DATE = datetime.datetime.now()
+DATE = timezone.now()
 STATUS_NAME = 'PASSED'
 STATUS_CODE = 1
 TEST_COMMENT = 'TestComment'
