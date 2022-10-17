55ed9b30a1f3dabd9f9af9d6589815f15ce36cc6
r.kabaev <Pfchfycbz2011>
2022-10-17 10:24:37 +0300

    Changed name of an err_msg

--- tests/error_messages.py
+++ tests/error_messages.py
@@ -1,6 +1,6 @@
 NOT_NULL_ERR_MSG = 'null value in column "{column}" of relation "{relation}" violates not-null constraint'
 CHAR_LENGTH_ERR_MSG = 'value too long for type character varying(255)'
-UNIQUE_KEY_ERR_MSG = 'Key ({column_name})=({column_value}) already exists.'
+ALREADY_EXISTS_ERR_MSG = 'Key ({column_name})=({column_value}) already exists.'
 INVALID_EMAIL_MSG = 'Enter a valid email address.'
 UNAUTHORIZED_MSG = 'Authentication credentials were not provided.'
 REQUIRED_FIELD_MSG = 'This field is required.'
