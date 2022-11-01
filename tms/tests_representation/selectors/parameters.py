f8c6fe16011641f4e973798e094e87625b06a721
r.kabaev <Pfchfycbz2011>
2022-11-01 12:26:42 +0300

    Added parameters by project endpoint.

--- tms/tests_representation/selectors/parameters.py
+++ tms/tests_representation/selectors/parameters.py
@@ -36,3 +36,6 @@ from tests_representation.models import Parameter
 class ParameterSelector:
     def parameter_list(self) -> QuerySet[Parameter]:
         return Parameter.objects.all()
+
+    def parameters_by_project_id(self, project_id: int) -> QuerySet[Parameter]:
+        return QuerySet(model=Parameter).filter(project=project_id).order_by('group_name')
