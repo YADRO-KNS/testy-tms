08777167df32bb3e4eb38ca69f77c96ca77ca1c6
r.kabaev <Pfchfycbz2011>
2022-11-08 17:19:57 +0300

    reverted commits

--- testy/scripts/makemessages.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-source /opt/testy/venv/bin/activate
-export DJANGO_SETTINGS_MODULE=testy.settings.development
-python testy/manage.py makemessages -l ru -e py -i venv
--- testy/scripts/start.dev.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-source /opt/testy/venv/bin/activate
-export DJANGO_SETTINGS_MODULE=testy.settings.development
-python testy/manage.py runserver
