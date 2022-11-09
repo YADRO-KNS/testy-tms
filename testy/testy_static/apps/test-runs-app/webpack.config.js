120c3eab4363bb8eaadf79929414a310a435a7fa
Dmitry Tkach <d.tkach@yadro.com>
2022-11-09 18:48:27 +0300

    Added building frontend applications in docker

--- testy/testy_static/apps/test-runs-app/webpack.config.js
+++ testy/testy_static/apps/test-runs-app/webpack.config.js
@@ -1,41 +1,43 @@
 const path = require("path");
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack')
 
 module.exports = (env) => {
-    return (
-        {
-            plugins: [
-                new Dotenv({
-                    path: `.env.${env.goal}`
-                }),
-            ],
-            entry: {
-                "test-runs-app": "./src/index.js"
+    return {
+        plugins: [
+            env.WEBPACK_SERVE
+                ? new Dotenv()
+                : new webpack.DefinePlugin({
+                    'process.env': JSON.stringify(process.env)
+                })
+        ],
+        entry: {
+            "test-runs-app": "./src/index.js"
+        },
+        output: {
+            path: path.resolve(__dirname, env.WEBPACK_SERVE ? '../../dist/assets/js/apps' : 'build'),
+            filename: "test-runs-app.js",
+        },
+        devServer: {
+            devMiddleware: {
+                writeToDisk: true,
             },
-            output: {
-                path: path.resolve(__dirname, '../../dist/assets/js/apps'),
-                filename: "test-runs-app.js",
-            },
-            devServer: {
-                devMiddleware: {
-                    writeToDisk: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /(node_modules)/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
                 },
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.jsx?$/,
-                        exclude: /(node_modules)/,
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env", "@babel/preset-react"]
-                        }
-                    },
-                    {
-                        test: /\.css$/i,
-                        use: ["style-loader", "css-loader"],
-                    },
-                ],
-            },
-        })
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+    }
 }
