271dcfd0e772d154b1239ddbe27477ec48c506c8
r.kabaev <Pfchfycbz2011>
2022-11-10 10:43:46 +0300

    fix

--- testy/testy/settings/development.py
+++ testy/testy/settings/development.py
@@ -35,7 +35,7 @@ DEBUG = True
 
 SECRET_KEY = 'django-insecure-97ml+ugrkdl6s!h)_5vanzw4%d_lajo6j(08e84e7314*&)s3)'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tms-dev.set.yadro.com']
 
 INSTALLED_APPS += [  # noqa F405
     'django_extensions',
