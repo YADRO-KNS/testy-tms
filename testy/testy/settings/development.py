2b0884ad62396a8165d55b5744a224aa65a4b570
r.kabaev <Pfchfycbz2011>
2022-11-16 11:29:24 +0300

    fix

--- testy/testy/settings/development.py
+++ testy/testy/settings/development.py
@@ -36,7 +36,7 @@ from testy.settings.common import *  # noqa F401, F403
 DEBUG = True
 
 SECRET_KEY = 'django-insecure-97ml+ugrkdl6s!h)_5vanzw4%d_lajo6j(08e84e7314*&)s3)'
-ALLOWED_HOSTS = json.loads(os.environ.get('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = json.loads(os.environ.get('ALLOWED_HOSTS', []))
 INSTALLED_APPS += [  # noqa F405
     'django_extensions',
 ]
