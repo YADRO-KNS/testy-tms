373f04649dd366bc592e41a113525591fb59abd2
r.kabaev <Pfchfycbz2011>
2022-11-21 14:08:04 +0300

    temp commit

--- testy/tests_description/api/v1/views.py
+++ testy/tests_description/api/v1/views.py
@@ -28,9 +28,10 @@
 # if any, to sign a "copyright disclaimer" for the program, if necessary.
 # For more information on this, and how to apply and follow the GNU AGPL, see
 # <http://www.gnu.org/licenses/>.
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from tests_description.api.v1.serializers import TestCaseSerializer, TestSuiteSerializer
+from tests_description.api.v1.serializers import TestCaseSerializer, TestSuiteSerializer, TestSuiteTreeSerializer
 from tests_description.selectors.cases import TestCaseSelector
 from tests_description.selectors.suites import TestSuiteSelector
 from tests_description.services.cases import TestCaseService
