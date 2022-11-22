55c7f54173d007f1f9ac86eb51ccd9299f6603d0
r.kabaev <Pfchfycbz2011>
2022-11-22 15:51:17 +0300

    Added filter backends

--- testy/testy/settings/common.py
+++ testy/testy/settings/common.py
@@ -196,6 +196,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
 
 # Company
