c7a3db6e814a1aa67a11a7a658658e264d792951
Dmitry Tkach <d.tkach@yadro.com>
2022-12-01 15:36:49 +0300

    Removed testy static

--- testy/testy/settings/common.py
+++ testy/testy/settings/common.py
@@ -170,13 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'testy_static/dist/assets',
-]
-
 STATIC_ROOT = 'static'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
