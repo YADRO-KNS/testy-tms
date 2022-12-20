e27a159412e873b3d8c46f89a1b5906b2b80c6a9
Dmitry Tkach <d.tkach@yadro.com>
2022-12-20 18:33:13 +0300

    Updated static settings

--- testy/testy/settings/common.py
+++ testy/testy/settings/common.py
@@ -164,7 +164,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/testy-static/'
 STATIC_ROOT = 'testy-static'
 
 # extensions should be specified with dot (.txt). If no extensions specified all extensions are allowed.
