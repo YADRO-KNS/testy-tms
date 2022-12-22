6345d720392440b4878be54f24a0c375fdcde6e2
Dmitry Tkach <d.tkach@yadro.com>
2022-12-22 09:50:04 +0300

    Updated django 3.2 -> 3.2.4, Added django-debug-toolbar

--- testy/testy/urls.py
+++ testy/testy/urls.py
@@ -89,3 +89,6 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
