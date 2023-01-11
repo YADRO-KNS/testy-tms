c4dfbd7cec2a2279d1597b3404f476a3725434ab
r.kabaev <Pfchfycbz2011>
2023-01-11 13:00:11 +0300

    linter fix

--- testy/plugins/migrator/testrail_migrator/migrator_lib/migrator_service.py
+++ testy/plugins/migrator/testrail_migrator/migrator_lib/migrator_service.py
@@ -28,8 +28,6 @@
 # if any, to sign a "copyright disclaimer" for the program, if necessary.
 # For more information on this, and how to apply and follow the GNU AGPL, see
 # <http://www.gnu.org/licenses/>.
-import logging
-import time
 from datetime import datetime
 
 import pytz
